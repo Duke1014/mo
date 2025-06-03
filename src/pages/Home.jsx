02b3f5767ff70659fe059e0187c9cd205b15b0cb
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-06-03 13:18:10 -0400

    body margin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,10 @@ import mo from "../assets/Mo Dance.jpg";
 export default function Home() {
   return (
     <div>
-        <h2>Actor, Musician, Home Owner, Cat Mother, Gemini, Friend, Roman, Countryman</h2>
+        {/* <h2>Actor, Musician, Home Owner, Cat Mother, Gemini, Friend, Roman, Countryman</h2> */}
         <div className='front-page-photo'>
           <h3>hello there wow</h3>
           <img src={mo} alt="Maureen dancing" />
-        
         </div>
     </div>
   )
